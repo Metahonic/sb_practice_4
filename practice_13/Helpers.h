acf5698ac6995ea18a9e29f28fcbdf9c4a966736
ViktorZelenko <victorbelka@gmail.com>
2024-05-25 06:33:54 +0300

    выполнил задание 13

---
+++ practice_13/Helpers.h
@@ -0,0 +1,5 @@
+int SquareSum(int a, int b)
+{
+	long long c = a + b;
+	return (c * c);
+}
---
+++ practice_13/practice_13.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+    long long a, b;
+
+    std::cout << "This program can calculate square of the sum of 2 entered numbers." << std::endl;
+
+    std::cout << "Enter the first number: ";
+    std::cin >> a;
+    std::cout << "Enter the second number: ";
+    std::cin >> b;
+
+    std::cout << "Square of the sum of entered numbers = " << SquareSum(a, b);
+    return 0;
+}
