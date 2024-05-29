7ac9af0d09962b99d4392efb164294c958e31835
ViktorZelenko <victorbelka@gmail.com>
2024-05-29 19:23:49 +0300

    упростил вычисление суммы элементов массива

--- practice_16/practice_16.cpp
+++ practice_16/practice_16.cpp
@@ -6,27 +6,28 @@ int main()
     const int N{ 6 };
     int array[N][N] = {};
 
+    struct tm buf;
+    time_t t = time(NULL);
+    localtime_s(&buf, &t);
+
+    int DivisionRemainder = (buf.tm_mday) % N;
+    int arrayStringSumm{ 0 };
+
     for (int i{ 0 }; i < N; i++)
     {
         for (int j{ 0 }; j < N; j++)
         {
             array[i][j] = i+j;
             std::cout << array[i][j] << " ";
+            if (j == DivisionRemainder)
+            {
+                arrayStringSumm = arrayStringSumm + j;
+            }
+
         }
         std::cout << "\n";
     }
-
-    struct tm buf;
-    time_t t = time(NULL);
-    localtime_s(&buf, &t);
     
-    int DivisionRemainder = (buf.tm_mday)%N;
-    int arrayStringSumm{0};
-    
-    for (int a{ 0 }; a < N; a++)
-    {
-        arrayStringSumm = arrayStringSumm + array[DivisionRemainder][a];
-    }
     std::cout << "\n";
     std::cout << "Summ of array elements with specified line index: " << arrayStringSumm << "\n";
     return 0;
