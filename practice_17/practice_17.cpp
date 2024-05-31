0b6edfcd4340e75b4ca7870aad4fb23cf749a582
ViktorZelenko <victorbelka@gmail.com>
2024-05-31 14:10:37 +0300

    выполнил задание 17

--- practice_17/practice_17.cpp
+++ practice_17/practice_17.cpp
@@ -1,26 +1,30 @@
 ﻿#include <iostream>
 #include <vector>
+#include <cmath>
 
 class Vector
 {
+private:
+    std::vector<int> Coordinates{0,1,0,1,0,1};
 public:
-    Vector() : x(1), y(1), z(1)
-    {}
-    Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
-    {}
-
     void Show()
     {
-        std::cout << "\n" << "X:" << x << ", " << "Y: " << y << ", " << "Z: " << z;
+        std::cout << "X1:" << Coordinates[0] << "; " << "X2:" << Coordinates[1] << "\n";
+        std::cout << "Y1:" << Coordinates[0] << "; " << "Y2:" << Coordinates[1] << "\n";
+        std::cout << "Z1:" << Coordinates[0] << "; " << "Z2:" << Coordinates[1] << "\n";
     }
-private:
-    double x{ 1 };
-    double y{ 1 };
-    double z{ 1 };
+    void VectorLength()
+    {
+        double a = sqrt(std::pow(Coordinates[1] - Coordinates[0], 2) + std::pow(Coordinates[3] - Coordinates[2], 2) + std::pow(Coordinates[5] - Coordinates[4], 2));
+        std::cout << "Vector length: " << a << "\n";
+    }
 };
 
 int main()
 {
-    Vector v;
-    v.Show();    
+    Vector Default;
+    Default.Show();
+    std::cout << "\n";
+    Default.VectorLength();
+    return 0;
 }
