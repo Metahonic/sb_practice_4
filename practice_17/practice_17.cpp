5fdc481faf20444249394665d37f7744d209b61c
ViktorZelenko <victorbelka@gmail.com>
2024-05-31 11:40:40 +0300

    завёл ветку для 17-го упражнения

---
+++ practice_17/practice_17.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+class Vector
+{
+public:
+    Vector() : x(1), y(1), z(1)
+    {}
+    Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
+    {}
+
+    void Show()
+    {
+        std::cout << "\n" << "X:" << x << ", " << "Y: " << y << ", " << "Z: " << z;
+    }
+private:
+    double x{ 1 };
+    double y{ 1 };
+    double z{ 1 };
+};
+
+int main()
+{
+    Vector v;
+    v.Show();    
+}
